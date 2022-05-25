d019e6ba0e43dc0d0e437e78d8cf6b95d00ceff4
Mustafa Hassan <mustafahassan.dev@gmail.com>
2022-05-25 13:53:39 +0200

    Style add ovelay img

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import img from './assets/img/1.jpg';
 import { getWeatherFor } from './modules/weather';
 
 const weatherImage = document.querySelector('.weather__image');
+const overlay = document.querySelector('.overlay');
 weatherImage.src = img;
-getWeatherFor('Khartoum');
+overlay.style.backgroundImage = `url('${img}')`;
